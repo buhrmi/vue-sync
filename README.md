a70179b420710875262723e7073002e4b1075496
buhrmi <buhrmi@gmail.com>
2016-09-30 13:21:23 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# vue-sync
+
+> Allows you to synchronize state of your vue components with other places.
+> Currently there are plans for different synchronization strategies:
+> 1. [x] Browser URL 
+> 2. [ ] Browser local storage
+> 3. [ ] Server API
+> 4. [ ] WebRTC
+
+## Install
+
+  $ npm install vue-sync
+  
+## Usage
+
+General usage
+
+    VueSync = require('vue-sync')
+    Vue.use(VueSync)
+    
+    new Vue({
+      data: {
+        someKey: 'someValue'
+      },
+      sync: {
+        someKey: syncStrategy
+      }
+    })
+
+### Sync Strategies
+
+#### Browser URL
+
+Sync state with browser URL
+
+    locationStrategy = VueSync.locationStrategy()
+    new Vue({
+      data: {
+        currentPage: 'users'
+      },
+      sync: {
+        currentPage: locationStrategy('page')
+      }
+    })
+    
+`locationStrategy(parameterName, noHistory = false)`
+
+* `parameterName`: (String) The parameter in the browser to sync with the key. For example, `page` will store the state in an URL that looks something like `http://somedomain.com/somesite/?page=users`
+* `noHistory`: (Boolean) Whether or not a browser history entry should be created every time the value changes. This enables or prevents the user to change Vue state using the navigation buttons of the browser.
+
+
+#### Local Storage
+
+Sync state with local storage
+
+> Coming soon
+
+#### Server API
+
+Sync state with a backend API
+
+> Coming soon
+
+#### WebRTC
+
+Sync state with other browsers (WebRTC)
+
+> Coming soon
---
+++ index.js
@@ -0,0 +1,117 @@
+(function () {
+  var locationStrategy = function() {
+    
+    var changeUrl, duringPopState, findParam, newValue, popHandler;
+    changeUrl = function(paramName, paramValue) {
+      var pattern, url;
+      url = window.location.toString();
+      pattern = new RegExp('\\b(' + paramName + '=).*?(&|$)');
+      if (url.search(pattern) >= 0) {
+        return url.replace(pattern, '$1' + paramValue + '$2');
+      } else {
+        return url + (url.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue;
+      }
+    };
+
+    findParam = function(paramName) {
+      var pattern, result, url;
+      url = window.location.toString();
+      pattern = new RegExp('\\b' + paramName + '=(.*?)(&|$)');
+      result = pattern.exec(url);
+      if ((result != null ? result.length : void 0) >= 1) {
+        return decodeURIComponent(result[1]);
+      }
+    };
+
+    duringPopState = false;
+
+
+    
+    return function(param, noHistory) {
+      return function(vm, path) {
+        popHandler = function() {
+          duringPopState = true;
+          newValue = findParam(param);
+          if (newValue) {
+            vm.$set(path, newValue);
+          }
+          vm.$nextTick(function() {
+            duringPopState = false;
+          });
+        };  
+
+        window.addEventListener('popstate', popHandler);
+        
+        newValue = findParam(param);
+
+        if (newValue) {
+          vm.$set(path, newValue);
+        }
+        else {
+          var newUrl = changeUrl(param, vm.$get(path));
+          history.replaceState(null, '', newUrl);
+        }
+        
+        vm.$watch(path, function(val1, val2) {
+          var newUrl;
+          if (duringPopState) {
+            return;
+          }
+          newUrl = changeUrl(param, val1);
+          
+          if (noHistory) {
+            history.replaceState(null, '', newUrl);
+          }
+          else {
+            history.pushState(null, '', newUrl);  
+          }
+        });
+        return function() {
+          // STOP syncing
+        }
+      }
+    }
+  }
+  
+
+
+  var vue // lazy bind
+
+  var sync = {
+    created: function () {
+      if (!vue) {
+        console.warn('[vue-sync] not installed!')
+        return
+      }
+    },
+    compiled: function() {
+      var syncOptions = this.$options.sync;
+      if (typeof syncOptions == 'function') syncOptions = syncOptions();
+      if (syncOptions) {
+        for (var key in syncOptions) {
+          if (syncOptions.hasOwnProperty(key)) {
+            var syncFn = syncOptions[key]
+            var stopFunc = syncFn(this, key);
+          } 
+        }   
+      }
+    }
+  }
+
+  var api = {
+    locationStrategy: locationStrategy,
+    mixin: sync,
+    install: function (Vue, options) {
+      vue = Vue
+      Vue.options = Vue.util.mergeOptions(Vue.options, sync)
+    }
+  }
+
+  if(typeof exports === 'object' && typeof module === 'object') {
+    module.exports = api
+  } else if(typeof define === 'function' && define.amd) {
+    define(function () { return api })
+  } else if (typeof window !== 'undefined') {
+    window.VueSync = api
+  }
+})()
